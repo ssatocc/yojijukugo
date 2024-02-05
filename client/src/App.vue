57af4b32d19d7f488031e630cb79b86110b44f8a
ssatocc <ssatocc@gmail.com>
2024-02-05 13:12:51 +0900

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>{{ node_env }}</h1>
+  <h3>{{ node_env }}</h3>
+  <h3>{{ node_env }}</h3>
 </template>
 
 <script>
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      node_env: ''
+    }
+  },
+  mounted() {
+    this.node_env = process.env.NODE_ENV
+    this.init()
+  },
+  methods: {
+    async init() {
+    }
   }
 }
 </script>
